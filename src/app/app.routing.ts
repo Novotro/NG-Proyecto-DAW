3c5ea72608f714d75f3c7ec5248f0b12a36ccd7e
Novotro <rubendr91@gmail.com>
2018-05-14 17:43:09 +0200

    Arreglo de las rutas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,8 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent} from './components/login/login.component';
 import { RegisterComponent} from './components/register/register.component';
 
-const appRoutes: Router = [
-  {path:'', component: LoginComponent},
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registro', component: RegisterComponent}
 ];
