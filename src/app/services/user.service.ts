47fcc7b5fa635879f0a6f00403755b7b00e7a964
Novotro <rubendr91@gmail.com>
2018-05-15 17:06:58 +0200

    Identificacion de usuarios y recuperacion del token

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -14,12 +14,26 @@ export class UserService{
     this.url = GLOBAL.url;
   }
 
+  //Servicio de Registro de usuario
   register(user: User) : Observable<any>{
     let params = JSON.stringify(user); //Convertimos el usuario a JSON
     let headers = new HttpHeaders().set('Content-Type', 'application/json');
 
 
     return this._http.post(this.url+'register', params, {headers: headers}); // Solicitud al servidor de la API
+  }
+
+  //Servicio de logeo de usuario
+  signup(user : User, gettoken= null) : Observable<any>{
+      if(gettoken != null){
+        user.gettoken = gettoken;
+        this.status = 'success';
+      }
+
+      let params = JSON.stringify(user); //Convertimos el usuario a JSON
+      let headers = new HttpHeaders().set('Content-Type', 'application/json'); //Defino las cabeceras HTTP
+
+      return this._http.post(this.url+'login', params, {headers: headers});
   }
 
 }
