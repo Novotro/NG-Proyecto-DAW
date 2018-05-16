60e233787418eff6933e6a94645d91fa39b76ada
Novotro <rubendr91@gmail.com>
2018-05-16 20:23:53 +0200

    Creacion del componente de gente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { LoginComponent} from './components/login/login.component';
 import { RegisterComponent} from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UsersComponent } from './components/users/users.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { UserEditComponent } from './components/user-edit/user-edit.component';
     LoginComponent,
     RegisterComponent,
     HomeComponent,
-    UserEditComponent
+    UserEditComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
