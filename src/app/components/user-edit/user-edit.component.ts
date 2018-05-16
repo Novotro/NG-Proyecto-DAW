9edcf98e8d4d61d8742e6679dcee8908ebf35fb5
Novotro <rubendr91@gmail.com>
2018-05-16 19:55:32 +0200

    Subida de foto de usuario

--- src/app/components/user-edit/user-edit.component.ts
+++ src/app/components/user-edit/user-edit.component.ts
@@ -3,11 +3,13 @@ import {Component, OnInit} from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
+import { UploadService } from '../../services/upload.service';
+import { GLOBAL } from '../../services/global';
 
 @Component({
-    selector 'user-edit',
+    selector: 'user-edit',
     templateUrl: './user-edit.component.html',
-    providers: [UserService]
+    providers: [UserService,UploadService]
 })
 
 export class UserEditComponent implements OnInit{
@@ -16,16 +18,19 @@ export class UserEditComponent implements OnInit{
     public identity;
     public token;
     public status:string;
+    public url: string;
 
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
-        private _userService: UserService
+        private _userService: UserService,
+        private _uploadService : UploadService
     ){
         this.title= 'Actualizar mis datos';
         this.user = this._userService.getIdentity();
         this.identity = this.user;
         this.token = this._userService.getToken();
+        this.url = GLOBAL.url;
 
     }
 
@@ -46,6 +51,11 @@ export class UserEditComponent implements OnInit{
                     this.identity = this.user;
 
                     //Subida de imagen de usuario
+                    this._uploadService.makeFileRequest(this.url+'upload-image-user/'+this.user._id, [], this.filesToUpload, this.token, 'image')
+                                       .then((result: any) =>{
+                                           this.user.image = result.user.image;
+                                           localStorage.setItem('identity', JSON.stringify(this.user));
+                                       });
                 }
             },
             error =>{
@@ -57,4 +67,11 @@ export class UserEditComponent implements OnInit{
             }
         );
     }
+    public filesToUpload: Array<File>;
+    fileChangeEvent(fileInput: any){
+        this.filesToUpload= <Array<File>>fileInput.target.files;
+    }
+
+
+
 }
