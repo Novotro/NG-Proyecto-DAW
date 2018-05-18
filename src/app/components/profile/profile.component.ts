e632d84ea6b73c93f25b94aab670b1812d56b223
Novotro <rubendr91@gmail.com>
2018-05-18 22:20:00 +0200

    Creacion de componente de usuario (perfil)

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,41 @@
+//Importe basico para crear un componente
+import {Component, OnInit} from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
+import { User } from '../../models/user';
+import { Follow } from '../../models/follow';
+import { UserService } from '../../services/user.service';
+import { GLOBAL } from '../../services/global';
+import { FollowService } from '../../services/follow.service';
+
+@Component({
+    selector:'profile',
+    templateUrl: './profile.component.html',
+    providers: [UserService, FollowService]
+})
+export class ProfileComponent implements OnInit{
+    public title: string;
+    public user : User;
+    public status : string;
+    public identity;
+    public token;
+    public url;
+    public stats;
+    public follow;
+
+
+    constructor(
+        private _route: ActivatedRoute,
+        private _router : Router,
+        private _userService : UserService,
+        private _followService : FollowService
+    ){
+        this.title = 'Perfil' ;
+        this.identity = this._userService.getIdentity();
+        this.token = this._userService.getToken();
+        this.url = GLOBAL.url;
+    }
+
+    ngOnInit(){
+        console.log('Profile.component cargado correctamente');
+    }
+}
