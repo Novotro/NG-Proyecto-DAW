aeb01ab6ce89173ac481a499baf031f747cc6c1d
Novotro <rubendr91@gmail.com>
2018-05-19 16:39:37 +0200

    Mostrar las publicaciones de un usuario en su mismo perfil

--- src/app/components/publications/publications.component.ts
+++ src/app/components/publications/publications.component.ts
@@ -1,5 +1,5 @@
 //Importe basico para crear un componente
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
 import { Publication } from '../../models/publication';
 import { GLOBAL } from '../../services/global';
@@ -25,6 +25,8 @@ export class PublicationsComponent implements OnInit{
     public itemsPerPage;
     public publications: Publication[];
 
+    @Input() user: string;
+
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
@@ -40,11 +42,11 @@ export class PublicationsComponent implements OnInit{
 
     ngOnInit(){
         console.log('Componente de Publications cargado');
-        this.getPublications(this.page);
+        this.getPublications(this.user,this.page);
     }
 
-    getPublications(page, adding =false){
-        this._publicationService.getPublications(this.token, page).subscribe(
+    getPublications(user, page, adding =false){
+        this._publicationService.getPublicationsUser(this.token, user, page).subscribe(
             response =>{
                 if(response.publications){
                     this.total = response.total_items;
@@ -84,7 +86,7 @@ export class PublicationsComponent implements OnInit{
             this.noMore = true;
         }else{
             this.page +=1;
-            this.getPublications(this.page, true);
+            this.getPublications(this.user, this.page, true);
         }
     }
 
