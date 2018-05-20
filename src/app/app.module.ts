e66a2725d867ea8aeb204ad806658f099a928b10
Novotro <rubendr91@gmail.com>
2018-05-20 15:01:45 +0200

    Listado de personas que nos siguen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { TimelineComponent } from './components/timeline/timeline.component';
 import { PublicationsComponent } from './components/publications/publications.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { FollowingComponent } from './components/following/following.component';
+import { FollowedComponent } from './components/followed/followed.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { FollowingComponent } from './components/following/following.component';
     TimelineComponent,
     PublicationsComponent,
     ProfileComponent,
-    FollowingComponent
+    FollowingComponent,
+    FollowedComponent
   ],
   imports: [
     BrowserModule,
