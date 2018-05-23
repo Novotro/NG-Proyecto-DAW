82ad2f344a948c5ac8caee8d51e4dcf25565501e
Novotro <rubendr91@gmail.com>
2018-05-23 19:07:43 +0200

    Mejoras en viajes, creacion del modelo, mejoras en eventos.

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -3,9 +3,11 @@ import {Component, OnInit} from '@angular/core';
 import { Router, ActivatedRoute, Params} from '@angular/router';
 import { User } from '../../models/user';
 import { Follow } from '../../models/follow';
+import { Travels } from '../../models/travels';
 import { UserService } from '../../services/user.service';
 import { GLOBAL } from '../../services/global';
 import { FollowService } from '../../services/follow.service';
+import { MouseEvent } from '@agm/core';
 
 @Component({
     selector:'maps',
@@ -22,8 +24,15 @@ export class MapsComponent implements OnInit{
     public stats;
     public followed;
     public following;
-    public lat: number = 51.678418;
-    public lng: number = 7.809007;
+    public  markers: marker[];
+    public letras = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    //Coordenadas de Barcelona
+    public lat: number = 41.524644;
+    public lng: number = 2.2679873;
+    public maximo = this.letras.length;
+    // google maps zoom level
+    public zoom: number = 8;
+
 
     constructor(
         private _route: ActivatedRoute,
@@ -37,6 +46,7 @@ export class MapsComponent implements OnInit{
         this.url = GLOBAL.url;
         this.following = false;
         this.followed = false;
+        this.markers = [];
     }
 
     ngOnInit(){
@@ -45,7 +55,34 @@ export class MapsComponent implements OnInit{
 
 
     onSubmit(){
-        
+
     }
 
+    mapClicked($event: MouseEvent) {
+        if(this.markers.length >= this.letras.length){
+            alert("No se pueden poner mas marcadores");
+        }else{
+            this.markers.push({
+                lat: $event.coords.lat,
+                lng: $event.coords.lng,
+                label: this.letras[this.markers.length],
+                draggable: true
+            });
+        }
+
+    }
+
+    //Metodo para conseguir las marcas del usuario logeado
+
+
+
+
+}
+
+
+interface marker {
+    lat: number;
+    lng: number;
+    label?: string;
+    draggable: boolean;
 }
---
+++ src/app/models/travels.ts
@@ -0,0 +1,6 @@
+export class Travels{
+  constructor(
+    public _id : string,
+    public markers: marker[]
+  ){}
+}
